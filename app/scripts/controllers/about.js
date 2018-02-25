78e4f06ffb7a64f38fb3b3bf0b8a0e78e5d388d2
Ullas Aithal <ullasaithalk@gmail.com>
2018-02-24 21:29:38 -0600

    Added weather from openweatherdata.com

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -8,10 +8,58 @@
  * Controller of the hackApp
  */
 angular.module('hackApp')
-  .controller('AboutCtrl', function () {
+  .controller('AboutCtrl', function ($scope, $http) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    $scope.weatherDate = [];
+    $scope.weatherFaranheit = [];
+    $scope.weatherMain = [];
+    $scope.weatherDesc = [];
+    $scope.weatherPic = [];
+
+    $http({
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      url: 'https://api.openweathermap.org/data/2.5/forecast?zip=60616&units=imperial&APPID=9c8e3a6d6f23e6fa92ad1c8265f49868'
+    }).then(function successCallback(response) {
+        var prevElement = null;
+        response.data["list"].forEach(element => {
+          if(prevElement != null){
+            
+            if(prevElement["dt_txt"].split(' ')[0] != element["dt_txt"].split(' ')[0]){
+              
+              $scope.weatherDate.push(element["dt_txt"].split(' ')[0]);
+              $scope.weatherFaranheit.push(element["main"]["temp"]);
+              $scope.weatherMain.push(element["weather"][0]["main"]);
+              $scope.weatherDesc.push(element["weather"][0]["description"]);
+              $scope.weatherPic.push("http://openweathermap.org/img/w/"+ element["weather"][0]["icon"] + ".png");
+    
+              prevElement = element;
+          }
+        }
+          else{
+
+            $scope.weatherDate.push(element["dt_txt"].split(' ')[0]);
+            $scope.weatherFaranheit.push(element["main"]["temp"]);
+            $scope.weatherMain.push(element["weather"][0]["main"]);
+            $scope.weatherDesc.push(element["weather"][0]["description"]);
+            $scope.weatherPic.push("http://openweathermap.org/img/w/"+ element["weather"][0]["icon"] + ".png");
+            prevElement = element;
+          }
+        });
+        console.log("Total Date",$scope.weatherDate);
+        console.log("Total Temp",$scope.weatherFaranheit);
+        console.log("Total type",$scope.weatherMain);
+        console.log("Total Des",$scope.weatherDesc);
+      }, function errorCallback(response) {
+        console.log("failed",response);
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+      });
   });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,24 @@
  * Controller of the hackApp
  */
 angular.module('hackApp')
-  .controller('MainCtrl', function () {
+  .controller('MainCtrl', function ($scope,$resource,$http) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-  });
+    $scope.abc = "text";
+    // var Weather = $resource('api.openweathermap.org/data/2.5/forecast?zip=60616&APPID=9c8e3a6d6f23e6fa92ad1c8265f49868');
+    // var weather = Weather.query(function(){
+    //     $scope.weatherResponse = weather;
+    //     console.log($scope.weather);
+    // });
+
+    
+
+  }
+
+   
+
+
+);
